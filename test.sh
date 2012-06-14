de9c1f56d9772e65a8ab7c18c1c61c46a3370fd1
Manish Kumar <manishk.life@gmail.com>
2012-06-14 16:55:40 +0530

    test commit to remove

--- test.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-echo "test result of echo $"
-echo " shows no of arguments.."
-echo $#
-echo " shows pid of current process.."
-echo $$
-echo " shows list of argument supplied.."
-echo $*
