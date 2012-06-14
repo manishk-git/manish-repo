d7e06717d154b3dc2a604c6a64c28f99e40d2771
Manish Kumar <manishk.life@gmail.com>
2012-06-14 17:01:10 +0530

    test commit to add test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+echo "test result of echo $"
+echo " shows no of arguments.."
+echo $#
+echo " shows pid of current process.."
+echo $$
+echo " shows list of argument supplied.."
+echo $*
